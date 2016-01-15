04555b6134f26656180f33273d907636d6643c8f
Mike Samuel <mikesamuel@gmail.com>
2016-01-15 08:54:10 -0500

    Visualizables: added commaList used to produce text form of output lists

--- src/main/java/com/google/template/autoesc/viz/Visualizables.java
+++ src/main/java/com/google/template/autoesc/viz/Visualizables.java
@@ -2,6 +2,9 @@ package com.google.template.autoesc.viz;
 
 import java.io.IOException;
 
+import com.google.common.collect.ImmutableList;
+import com.google.template.autoesc.out.Output;
+
 /**
  * Factories for instances of Visualizable.
  */
@@ -13,15 +16,56 @@ public final class Visualizables {
   public static Visualizable text(String s) {
     return new TextVisualizable(s);
   }
+
+  /**
+   * A visualizable that puts commas between the elements of {@code ls}.
+   */
+  public static Visualizable commaList(Iterable<? extends Output> ls) {
+    return new CommaListVisualizable(ls);
+  }
 }
 
 final class TextVisualizable implements Visualizable {
   final String s;
+
   TextVisualizable(String s) {
     this.s = s;
   }
+
   @Override
   public void visualize(DetailLevel lvl, VizOutput out) throws IOException {
     out.text(s);
+  }
+
+  @Override
+  public String toString() {
+    return TextVizOutput.vizToString(this, DetailLevel.LONG);
+  }
+}
+
+final class CommaListVisualizable extends AbstractVisualizable {
+  final ImmutableList<Visualizable> ls;
+
+  CommaListVisualizable(Iterable<? extends Visualizable> ls) {
+    this.ls = ImmutableList.copyOf(ls);
+  }
+
+  @Override
+  protected String getVizTypeClassName() {
+    return "list";
+  }
+
+  @Override
+  protected void visualizeBody(DetailLevel lvl, VizOutput out)
+      throws IOException {
+    boolean first = true;
+    for (Visualizable v : ls) {
+      if (first) {
+        first = false;
+      } else {
+        out.text(", ");
+      }
+      v.visualize(lvl, out);
+    }
   }
 }
